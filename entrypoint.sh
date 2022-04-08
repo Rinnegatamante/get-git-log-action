0dedcdc737a9faf880521b4f4909d883eaa9953b
Rinnegatamante <rinnegatamante@gmail.com>
2022-04-08 17:54:00 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 x = 0
 git log --pretty=format:'* %h %s' | while read line ; do echo ::set-output name=log${x}::"$line" ; x=$(( $x + 1 ));  done
-
-echo "$log"
-echo ::set-output name=log::"$log"
-
